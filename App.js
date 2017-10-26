05a255d051e9779bfd201afc777759f9af9b99f4
daishi <daishi@axlight.com>
2017-10-26 19:04:29 +0900

    use graph.cool

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 import TodoList from './TodoList';
 
 const link = new HttpLink({
-  uri: 'https://api.graphcms.com/simple/v1/cj95kuyzo1cwd012521echyb4',
+  uri: 'https://api.graph.cool/simple/v1/cj98a57z515m50115c8of9cu7',
 });
 const client = new ApolloClient({ link });
 
