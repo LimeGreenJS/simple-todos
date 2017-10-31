81f670bf60cc81b64710fb61c8b06c3faae866d8
Daishi Kato <dai-shi@users.noreply.github.com>
2017-10-31 22:03:13 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
 import TodoList from './TodoList';
@@ -8,7 +9,8 @@ import TodoList from './TodoList';
 const link = new HttpLink({
   uri: 'https://api.graph.cool/simple/v1/cj98a57z515m50115c8of9cu7',
 });
-const client = new ApolloClient({ link });
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
 
 const App = () => (
   <ApolloProvider client={client}>
