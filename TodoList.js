c51744b297b0fafc310791bc0f3239377b57a7be
daishi <daishi@axlight.com>
2018-04-05 15:34:31 +0900

    to avoid undefined error

--- TodoList.js
+++ TodoList.js
@@ -5,20 +5,17 @@ import { graphql } from 'react-apollo';
 import TodoItem from './TodoItem';
 import NewTodoForm from './NewTodoForm';
 
-const TodoList = ({ data: { loading, error, allTasks } }) => {
-  console.log(allTasks);
-  return (
-    loading ? <p>Loading...</p> :
-    error ? <p>Error: {error.message}</p> : (
-      <ul>
-        <li><NewTodoForm /></li>
-        {allTasks.map(task => (
-          <li key={task.id}><TodoItem item={task} /></li>
-        ))}
-      </ul>
-    )
-  );
-};
+const TodoList = ({ data: { loading, error, allTasks = [] } }) => (
+  loading ? <p>Loading...</p> :
+  error ? <p>Error: {error.message}</p> : (
+    <ul>
+      <li><NewTodoForm /></li>
+      {allTasks.map(task => (
+        <li key={task.id}><TodoItem item={task} /></li>
+      ))}
+    </ul>
+  )
+);
 
 export const QUERY_ALL_TASKS = gql`
 {
