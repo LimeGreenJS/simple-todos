e1719dc0eacd835a7231effd25f8e7892c5251ab
daishi <daishi@axlight.com>
2018-04-05 15:37:23 +0900

    for debug

--- TodoList.js
+++ TodoList.js
@@ -5,17 +5,22 @@ import { graphql } from 'react-apollo';
 import TodoItem from './TodoItem';
 import NewTodoForm from './NewTodoForm';
 
-const TodoList = ({ data: { loading, error, allTasks = [] } }) => (
-  loading ? <p>Loading...</p> :
-  error ? <p>Error: {error.message}</p> : (
-    <ul>
-      <li><NewTodoForm /></li>
-      {allTasks.map(task => (
-        <li key={task.id}><TodoItem item={task} /></li>
-      ))}
-    </ul>
-  )
-);
+const TodoList = (props) => {
+  console.log('-----------', props);
+  const { data } = props;
+  const { loading, error, allTasks } = data;
+  return (
+    loading ? <p>Loading...</p> :
+    error ? <p>Error: {error.message}</p> : (
+      <ul>
+        <li><NewTodoForm /></li>
+        {allTasks.map(task => (
+          <li key={task.id}><TodoItem item={task} /></li>
+        ))}
+      </ul>
+    )
+  );
+};
 
 export const QUERY_ALL_TASKS = gql`
 {
